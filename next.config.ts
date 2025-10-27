faec0653d49a20bbf7812a9763bc1ec5c7219ccc
jose <pucciarellijose@gmail.com>
2025-10-27 17:56:22 -0300

    ignore eslint and typescript during builds

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Ensure Next resolves modules/plugins relative to this project, not a parent dir
   outputFileTracingRoot: __dirname,
-    eslint: {
-        ignoreDuringBuilds: true,
-    }, typescript: {
-        ignoreBuildErrors: true
-    }
+  eslint: {
+      ignoreDuringBuilds: true,
+  }, typescript: {
+      ignoreBuildErrors: true
+  }
 };
 
 export default nextConfig;
